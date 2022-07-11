1cf0e1451f79be3c74db1a4bac4c33cca90ee1bf
muhamad sadiq <solinsadiq11@gmail.com>
2022-07-12 01:07:18 +0300

    Learned chaining functions with andThen

--- src/main/java/_Function.java
+++ src/main/java/_Function.java
@@ -4,12 +4,20 @@ public class _Function {
 
     public static void main(String[] args) {
         System.out.println(incrementByOne(2));
-        System.out.println(incrementByOneFunction.apply(2));
+
+        //normall
+        int result=incrementByOneFunction.apply(2);
+        System.out.println(result);
+        System.out.println(multiplyByTenFunction.apply(result));
+
+        //using andThen method
+        System.out.println(incrementByOneFunction.andThen(multiplyByTenFunction).apply(2));
     }
 
 
     static Function<Integer,Integer> incrementByOneFunction = number -> number+1;
-    //normal function
+    static Function<Integer,Integer> multiplyByTenFunction = number -> number*10;
+
     public static int incrementByOne(int number){
         return number+1;
     }
