0b00d47b6de970e43cebcfbea35be18ee5db61ef
muhamad sadiq <solinsadiq11@gmail.com>
2022-07-12 02:03:14 +0300

    Learned Consumer functional interface

---
+++ src/main/java/_Consumer.java
@@ -0,0 +1,37 @@
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+            Customer customer = new Customer("muhammed",9172896);
+            greetCustomer(customer);
+
+            greetCustomerConsumer.accept(customer);
+    }
+
+
+
+
+    //normal java function
+    public static void greetCustomer(Customer customer){
+        System.out.println("Hello "+customer.customerName
+                +", thanks for registering phone number "
+                + customer.customerPhoneNumber);
+    }
+    //Consumer functional interface
+    static Consumer<Customer> greetCustomerConsumer = customer
+            -> System.out.println("Hello "+customer.customerName
+            +", thanks for registering phone number "
+            + customer.customerPhoneNumber);
+
+    static class Customer{
+        private final String customerName;
+        private final int customerPhoneNumber;
+
+
+        Customer(String customerName, int customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
